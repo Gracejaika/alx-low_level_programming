f1f5ca6f5e4fb5eaf04ea8c35557ee79ffe8485a
Gracejaika <gracejaika1@gmail.com>
2022-06-14 15:34:48 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,10 @@
 
 int main (void)
 {
-	print_alphabet();
-	return (0);
-}
-void print_alphabet()
-{
 	char alphabet;
 	for(alphabet= 'a'; alphabet<='z'; alphabet++)
-	      _putchar(alphabet);
-	_putchar('\n');	
+		putchar(alphabet);
+	putchar('\n');
+
+	return (0);
 }
